e2210c5129f7dc933c22a8d6b89a8b897f1692b5
wellington barbosa <carneirobarbosawellington@gmail.com>
2021-06-22 09:53:33 -0300

    App background linear

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
 import { SignIn } from './src/screens/SignIn';
+import { Background } from './src/components/Background';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,14 +21,14 @@ export default function App() {
   }
 
   return (
-    <Fragment>
+    <Background>
       <StatusBar 
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-          />
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
       <SignIn />
-    </Fragment>
+    </Background>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -5,8 +5,7 @@ export const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: theme.colors.secondary90
+      alignItems: 'center'
     },
 
     image: {
